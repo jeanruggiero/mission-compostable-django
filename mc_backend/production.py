a4069369c540809eca9bfbff8f27e32b8da30974
Jean Ruggiero <jeanruggiero@gmail.com>
2020-02-09 01:18:50 -0500

    Added production settings

---
+++ mc_backend/production.py
@@ -0,0 +1,17 @@
+import os
+from .settings import *
+
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'),
+                 'localhost',
+                 'mission-compostable.herokuapp.com',
+                 'missioncompostable.online',
+                 'www.missioncompostable.online']
+
+#DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
